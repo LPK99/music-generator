41fa68f4a0b7ddda16e3d3587cfade068331cf07
Your Name <you@example.com>
2023-09-16 23:37:24 +0700

    updated app.py added clear_cuda_memory function

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 from audiocraft.models import MusicGen
+import torch
+import gc
 
 input_list = []
 
-@st.cache_resource(show_spinner=False)
+
+def clear_cuda_memory():
+    torch.cuda.empty_cache()
+    st.cache_resource.clear()
+    torch.cuda.empty_cache()
+    gc.collect()
+
+@st.cache_resource()
 def load_model(duration):
     duration_ = int(duration)
     model = MusicGen.get_pretrained("melody")
@@ -21,6 +30,12 @@ def main():
         page_title="Music Generator",
         page_icon="musical-note.png",
     )
+    cache = torch.cuda.memory_cached() / 1024 ** 3
+    print(cache)
+    if cache >= 4.6 :
+        clear_cuda_memory()
+    print(f"CUDA Memory Allocated: {torch.cuda.memory_allocated() / 1024 ** 3:.2f} GB")
+    print(f"CUDA Memory Cached: {torch.cuda.memory_cached() / 1024 ** 3:.2f} GB")
     st.title('Music Generator')
     st.image(image='musical-note.png', width=100)
     prompt_input = st.text_input('Enter your music description')
