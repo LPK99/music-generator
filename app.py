f3304f89b67683e3217cba6da559c0dfc53f9f27
Your Name <trungducmic@gmail.com>
2023-09-27 23:18:47 +0700

    updated app.py

--- app.py
+++ app.py
@@ -2,31 +2,27 @@ import streamlit as st
 from audiocraft.models import MusicGen
 import torch
 import gc
-
-input_list = []
-
-from pedalboard.io import AudioFile
 from pedalboard import *
 import noisereduce as nr
 
+input_list = []
 def improve(audio):
   
+    sr = 44100
 
-  sr = 44100
 
+    reduced_noise = nr.reduce_noise(y=audio, sr=sr, stationary=True, prop_decrease=0.75)
 
-  reduced_noise = nr.reduce_noise(y=audio, sr=sr, stationary=True, prop_decrease=0.75)
+    board = Pedalboard([
+        NoiseGate(threshold_db=-30, ratio=1.5, release_ms=250),
+        Compressor(threshold_db=-16, ratio=2.5),
+        LowShelfFilter(cutoff_frequency_hz=400, gain_db=10, q=1),
+        Gain(gain_db=10)
+    ])
 
-  board = Pedalboard([
-    NoiseGate(threshold_db=-30, ratio=1.5, release_ms=250),
-    Compressor(threshold_db=-16, ratio=2.5),
-    LowShelfFilter(cutoff_frequency_hz=400, gain_db=10, q=1),
-    Gain(gain_db=10)
-  ])
-
-  effected = board(reduced_noise, sr)
+    effected = board(reduced_noise, sr)
   
-  return effected
+    return effected
 
 
 
